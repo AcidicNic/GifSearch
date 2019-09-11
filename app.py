66410f35edb5aa2d45feffcc520a509ab3c48624
snekek <nic4096@gmail.com>
2019-09-10 19:04:27 -0700

    gifs work now

--- app.py
+++ app.py
@@ -8,26 +8,31 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
+    # try:
     query = request.args.get('query')
+
 
     # TODO: Make 'params' dict with query term and API key
     params = {
-        "api": "566RIQJP8K3Q",
-        "query": query
+        "q": query,
+        "Key": "566RIQJP8K3Q",
+        "lmt" : 10
     }
 
     # TODO: Make an API call to Tenor using the 'requests' library
-    r = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params["query"], params["api"], 10))
+    r = requests.get('https://api.tenor.com/v1/search', params = params)
 
     if not r.status_code == 200:
         print("error")
 
     # TODO: Get the first 10 results from the search results
-    top10gifs = json.loads(r.content)
+    gifs_json = r.json()
+    gifs = gifs_json["results"]
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    print (gifs)
 
-    return render_template("index.html", gif = top10gifs)
+    return render_template("index.html", gifs = gifs)
 
 
 if __name__ == '__main__':
